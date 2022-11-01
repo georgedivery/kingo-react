29333255f35b0f1da6622026b5969cd97e43ac1d
George Divery <georgedivery@gmail.com>
2022-11-01 12:15:43 +0200

    .

--- src/App.js
+++ src/App.js
@@ -41,7 +41,6 @@ function App() {
             .then(res => setContractAddress(res))
             .catch(err => { 
                 setServerErr(true)
-                setContractAddress(err.message)
             })
     }, [])
 
