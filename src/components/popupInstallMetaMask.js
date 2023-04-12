e659e372447139911eb2b4451981eff878a94f1f
George Divery <georgedivery@gmail.com>
2023-04-12 10:24:25 +0300

    update popup install meta

--- src/components/popupInstallMetaMask.js
+++ src/components/popupInstallMetaMask.js
@@ -1,42 +1,17 @@
-function NotMetamaskMessage({ message, handleClosePopupInstallMetamask }) {
-    return (
-        <div className="popup_install_metamask active">
-            <div className="popup_content">
-                <div className="popup_content_inner">
-                    <h4 className="text_center">{message}</h4>
-                    <div className="btn_container">
-                        <button
-                            onClick={handleClosePopupInstallMetamask}
-                            className="btn btn_small close"
-                        >
-                            OK
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
-
 function RedirectToMetamask({handleClosePopupInstallMetamask}) {
     handleClosePopupInstallMetamask();
     window.location.href = 'https://metamask.app.link/dapp/kingo.ai/ptrn/'; 
-  };
+};
 
-function PopupInstallMetaMask({ handleClosePopupInstallMetamask, message }) {
+
+function PopupInstallMetaMask({
+    handleClosePopupInstallMetamask
+}) {
 
     return (
-        <>
-            {message !== "Please intall MetaMask!"?
-            <NotMetamaskMessage
-                    handleClosePopupInstallMetamask={
-                        handleClosePopupInstallMetamask
-                    }
-                    message={message}
-                />:<RedirectToMetamask handleClosePopupInstallMetamask={handleClosePopupInstallMetamask}/>
-            }
-        </>
+        <RedirectToMetamask handleClosePopupInstallMetamask={handleClosePopupInstallMetamask}/>
     );
 }
+
 
 export default PopupInstallMetaMask;
